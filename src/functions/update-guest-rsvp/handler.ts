1408e768842144d272b29e5b00c9ed1fd1a1b5ad
yoadwo <yoadw20@gmail.com>
2023-05-25 11:09:02 +0300

    change: query cols attendingCount, lastUpdated

--- src/functions/update-guest-rsvp/handler.ts
+++ src/functions/update-guest-rsvp/handler.ts
@@ -6,9 +6,8 @@ import { makeDb } from '@libs/promisify-mysql';
 import schema from './schema';
 
 const updateGuestRsvp: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const code = event.pathParameters?.inviteCode;
-
-  if (event.body.inviteCode != code) {
+  
+  if (event.body.inviteCode != event.pathParameters?.inviteCode) {
     return formatBadRequestResponse({ message: 'Bad request', event })
   }
 
@@ -16,11 +15,13 @@ const updateGuestRsvp: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async
 
   try {
     const DbOkResponse = await db.query(
-      "UPDATE guests SET attendingNumber = ?, status = ? where phoneNumberHash = ?",
-      [event.body.attending, event.body.status, event.body.inviteCode]);    
+      "UPDATE guests_extended_2 SET attendingCount = ?, status = ?, lastUpdated = ? where phoneNumberHash = ?",
+      [event.body.attending, event.body.status, currentTimeToMysqlFormat(), event.body.inviteCode]);    
     if (DbOkResponse.affectedRows == 0){
+      console.warn(`weirdly, no such guests with code ${event.body.inviteCode} exists`)
       return formatBadRequestResponse({data: {message: "No such guest", code: -1}});    
     } else {
+      console.log(`guest ${event.body.inviteCode} updated with status ${event.body.status}`)
       return formatOkResponse({data: {message: "Guest updated", code: 0}});
     }    
   } catch (err) {
@@ -30,5 +31,9 @@ const updateGuestRsvp: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async
     await db.close();
   }
 };
+
+function currentTimeToMysqlFormat() {
+  return new Date().toISOString().slice(0, 19).replace('T', ' ');
+}
 
 export const main = middyfy(updateGuestRsvp);
